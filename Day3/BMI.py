b4711af81bf172ba99b4a2885261595c747c97a9
Claudio Ferreira Junior <claudioferreirajr@hotmail.com>
2024-03-30 13:17:18 -0300

    fix: bmi name variable

--- Day3/BMI.py
+++ Day3/BMI.py
@@ -6,15 +6,15 @@ weight = int(input())
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
-BMI = round((weight/height**2), 1)
+bmi = round((weight/height**2), 1)
 
-if BMI < 18.5:
-  print(f"Your BMI is {BMI}, you are underweight.")
-elif BMI < 25:
-  print(f"Your BMI is {BMI}, you have a normal weight.")
-elif BMI < 30:
-  print(f"Your BMI is {BMI}, you are slightly overweight.")
-elif BMI < 35:
-  print(f"Your BMI is {BMI}, you are obese.")
+if bmi < 18.5:
+  print(f"Your BMI is {bmi}, you are underweight.")
+elif bmi < 25:
+  print(f"Your BMI is {bmi}, you have a normal weight.")
+elif bmi < 30:
+  print(f"Your BMI is {bmi}, you are slightly overweight.")
+elif bmi < 35:
+  print(f"Your BMI is {bmi}, you are obese.")
 else:
-  print(f"Your BMI is {BMI}, you are clinically obese.")
+  print(f"Your BMI is {bmi}, you are clinically obese.")
