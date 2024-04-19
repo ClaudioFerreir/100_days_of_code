7fe54ab1cb3159fe37ec3e724b61dd6c7e485cc9
Claudio Ferreira Junior <claudioferreirajr@hotmail.com>
2024-04-18 23:04:02 -0300

    feat: highest score add

---
+++ Day5/highest_score.py
@@ -0,0 +1,10 @@
+# Input a list of student scores
+student_scores = [78, 65, 89, 86, 55, 91, 64, 89]
+
+# Write your code below this row 👇
+highest_score = 0
+for score in student_scores:
+  if score > highest_score:
+    highest_score = score
+
+print(f"The highest score in the class is: {highest_score}")
