ca63082dcdb4ca4d927e088106c843ee04473943
Claudio Ferreira Junior <claudioferreirajr@hotmail.com>
2024-10-30 17:28:57 -0300

    fix: main.py Day9

--- Day9_Blind_Auction_Project/main.py
+++ Day9_Blind_Auction_Project/main.py
@@ -2,32 +2,38 @@ import art
 
 print(art.logo)
 print("Welcome to the secret auction program.")
+
+# TODO-4: Compare bids in dictionary
+def find_highest_bidder(bidding_dictionary):
+    best_bidder = None
+    highest_bid = 0
+
+    for key in bidding_dictionary:
+        if bidding_dictionary[key] > highest_bid:
+            best_bidder = key
+            highest_bid = bids[key]
+
+    print(f"The winner is {best_bidder} with a bid of ${highest_bid}.")
+
 # TODO-1: Ask the user for input
-bid_list = {}
-other_bid = True
+bids = {}
+should_continue = True
 
 def blind_auction():
     name = input("What is your name?: ")
-    price = int(input("What is your bid?: $ "))
+    price = int(input("What is your bid?: $"))
     # TODO-2: Save data into dictionary {name: price}
-    bid_list[name] = price
+    bids[name] = price
     # TODO-3: Whether if new bids need to be added
 
-while other_bid:
+while should_continue:
     blind_auction()
-    other_bid = input("Are there any other bidders? Type 'yes' or 'no':")
-    if other_bid == "no":
-        other_bid = False
-    print("\n" * 20)
+    should_continue = input("Are there any other bidders? Type 'yes' or 'no'. \n").lower()
+
+    if should_continue == "no":
+        should_continue = False
+        find_highest_bidder(bids)
+    elif should_continue == "yes":
+        print("\n" * 20)
 
 
-# TODO-4: Compare bids in dictionary
-best_bidder = None
-highest_bid = 0
-
-for key in bid_list:
-    if bid_list[key] > highest_bid:
-        best_bidder = key
-        highest_bid = bid_list[key]
-
-print(f"{best_bidder} won with the $ {highest_bid}.")
