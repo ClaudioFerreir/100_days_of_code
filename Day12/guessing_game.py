1a71e5fcdbfa3e5d621891f9a25907c28d62052c
Claudio Ferreira Junior <claudioferreirajr@hotmail.com>
2024-11-05 23:45:40 -0300

    fix: return functions

--- Day12/guessing_game.py
+++ Day12/guessing_game.py
@@ -1,8 +1,11 @@
 import random
 
+from art import logo
+
 def game():
+
+    print(logo)
     number = random.randint(1, 100)
-    print(number)
 
     print("Welcome to the Number Guessing Game!")
     print("I'm thinking of a number between 1 and 100.")
@@ -11,20 +14,39 @@ def game():
 
     if difficulty == "easy":
         attempts = 10
-    else:
+    elif difficulty == "hard":
         attempts = 5
+    else:
+        attempts = 0
+        print("That's not a valid option. Refresh the page to run again!")
+
+    def guess_again(n):
+        if n != 1:
+            print("Guess again.")
 
     # Here starts the looping
-    print(f"You have {attempts} attempts to guess the number.")
+    for i in range(attempts):
+        print(f"You have {attempts} attempts remaining to guess the number.")
 
-    guess = int(input("Make a guess: "))
+        guess = int(input("Make a guess: "))
 
-    if guess == number:
-        print("You are correct!")
-    elif guess > number:
-        print("Your guess is too high.")
-    else:
-        print("Your guess is too low.")
+        if guess == number:
+            print(f"You got it! The answer was {number}.")
+            return
+        elif guess > number and attempts > 1:
+            print("Too high.")
+            guess_again(attempts)
+        else:
+            if attempts > 1:
+                print("Too low.")
+                guess_again(attempts)
+
+        attempts -= 1
+
+        if attempts == 0:
+            print("You've run out of guesses. Refresh the page to run again!!!")
+            print(f"The number was {number}.")
+            return
 
 
 game()
