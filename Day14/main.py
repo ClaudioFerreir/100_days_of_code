eb398e6d4cae3d1f47b506c49046234dc5a5ad7f
Claudio Ferreira Junior <claudioferreirajr@hotmail.com>
2024-11-07 16:36:30 -0300

    feat: logic struture work

--- Day14/main.py
+++ Day14/main.py
@@ -1,10 +1,44 @@
 from random import randint
 
 from game_data import data
+from art import logo, vs
 
-# Generate a random number into the game data index
+
 def take_item():
-    index = randint(0, len(data)-1)
-    return data[index]
+    """Refer a item into the game_data"""
+    index = randint(0, len(data) - 1)
+    item = data[index]
+    return item
 
-print(take_item())
+def game():
+    item_a = take_item()
+    item_b = take_item()
+    score = 0
+
+    next_round = True
+
+    while next_round:
+        print(logo)
+
+        if score != 0:
+            print(f"You're right! Current score: {score}.")
+
+        print(f"Compare A: {item_a['name']}, a {item_a['description']}, from {item_a['country']}.")
+        print(vs)
+        print(f"Against B: {item_b['name']}, a {item_b['description']}, from {item_b['country']}.")
+
+        option = input("Who has more followers? Type 'A' or 'B': ").upper()
+
+        if option == 'A' and item_a['follower_count'] > item_b['follower_count']:
+            item_b = take_item()
+            score += 1
+
+        elif option == 'B' and item_b['follower_count'] > item_a['follower_count']:
+            item_a = item_b
+            item_b = take_item()
+            score += 1
+        else:
+            print(f"Sorry, that's wrong. Final score: {score}!")
+            next_round = False
+
+game()
